ff73e7cfd7bb0351e65d732cc0f69d321e9254ed
goldbattle <patfgeneva@gmail.com>
2013-03-08 22:09:46 -0500

    Added Server Polling; Moved Buttons
    todo
    =server info layout
    =connect buttons

--- src/mod/ares/gui/Ares_ServerGUI.java
+++ src/mod/ares/gui/Ares_ServerGUI.java
@@ -1,5 +1,7 @@
 package mod.ares.gui;
 
+import java.util.ArrayList;
+
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityClientPlayerMP;
 import net.minecraft.client.gui.FontRenderer;
@@ -18,46 +20,107 @@ import net.minecraft.util.StatCollector;
 import net.minecraft.util.StringTranslate;
 
 public class Ares_ServerGUI extends GuiScreen {
+	ArrayList<Ares_ThreadPollServers> serverList;
+
 	/**
 	 * Default constructor
 	 */
 	public Ares_ServerGUI() {
-
+		//Construct the server polls here.
+		serverList = new ArrayList<Ares_ThreadPollServers>();
+		//add all the servers
+		serverList.add(new Ares_ThreadPollServers("alpha.oc.tc",25565));
+		serverList.add(new Ares_ThreadPollServers("beta.oc.tc",25565));
+		serverList.add(new Ares_ThreadPollServers("gamma.oc.tc",25565));
+		serverList.add(new Ares_ThreadPollServers("delta.oc.tc",25565));
+		serverList.add(new Ares_ThreadPollServers("epsilon.oc.tc",25565));
+		serverList.add(new Ares_ThreadPollServers("zeta.oc.tc",25565));
+		serverList.add(new Ares_ThreadPollServers("eta.oc.tc",25565));
+		serverList.add(new Ares_ThreadPollServers("theta.oc.tc",25565));
+		serverList.add(new Ares_ThreadPollServers("iota.oc.tc",25565));
+		serverList.add(new Ares_ThreadPollServers("kappa.oc.tc",25565));
+		serverList.add(new Ares_ThreadPollServers("lambda.oc.tc",25565));
+		serverList.add(new Ares_ThreadPollServers("nostalgia.oc.tc",25565));
+		//update all their info
+		Thread thread = new Thread() {
+			public void run() {
+				runServerPolls();
+			}
+		};
+		thread.start();
 	}
 
 	/**
-	 * This method is a override method for drawing a gui
-	 * All "painting" should take place in here
+	 * This is init of the gui when it is about to get drawn. You should only
+	 * have buttons/control elements in here.
+	 */
+	public void initGui() {
+		// clear the list
+		this.controlList.clear();
+		// top row buttons
+		this.controlList.add(new GuiButton(2, this.width - (3*75 + 16),
+				5, 75, 20, "Refresh"));
+		this.controlList.add(new GuiButton(1, this.width - (2*75 + 13),
+				5, 75, 20, "Old Menu"));
+		this.controlList.add(new GuiButton(0, this.width - (75 + 10),
+				5, 75, 20, "Cancel"));
+	}
+
+	/**
+	 * This method is a override method for drawing a gui All "painting" should
+	 * take place in here
 	 */
 	public void drawScreen(int x, int y, float f) {
-		this.controlList.clear();
 		drawDefaultBackground();
+		drawGradientRect(5, 3, this.width-5, 20+6, 1615855616, -1602211792);
+		//drawGradientRect(5, 3, this.width-5, 20+3, -1073741824,-1073741824);
+		// title
+		drawString(this.fontRenderer, "Project Ares Servers", 10, 10, 16777215);
 
-		// page title
-		drawString(this.fontRenderer, "Project Ares Servers", 10, 8, 16777215);
-		
-		// side buttons
-		this.controlList.add(new GuiButton(1, this.width - (75 + 5),
-				this.height - 50, 75, 20, "Old Menu"));
-		this.controlList.add(new GuiButton(0, this.width - (75 + 5),
-				this.height - 28, 75, 20, "Cancel"));
+		// server info drawing
+		//drawString(this.fontRenderer, "§4" + alpha.serverIP, 10, 30, 16777215);
+		//drawString(this.fontRenderer, alpha.gameVersion, 10, 20, 16777215);
+		//drawString(this.fontRenderer, alpha.MOTD, 10, 28, 16777215);
+		//drawString(this.fontRenderer, alpha.pingToServer, 10, 36, 16777215);
+		//drawString(this.fontRenderer, alpha.populationInfo, 10, 45, 16777215);
 
 		// tells the super class to paint everything
 		super.drawScreen(x, y, f);
 	}
 
 	/**
-	 * If a button is clicked this method gets called. 
-	 * The id is the number given to the button during init.
+	 * If a button is clicked this method gets called. The id is the number
+	 * given to the button during init.
 	 */
 	protected void actionPerformed(GuiButton par1GuiButton) {
 		if (par1GuiButton.enabled) {
+			// cancel
 			if (par1GuiButton.id == 0) {
 				this.mc.displayGuiScreen(new GuiMainMenu());
-			} else if (par1GuiButton.id == 1) {
+			}
+			// old menu
+			else if (par1GuiButton.id == 1) {
 				GuiListener.toggleMultiGUI(false);
 				this.mc.displayGuiScreen(new GuiMultiplayer(new GuiMainMenu()));
 			}
+			// refresh
+			else if (par1GuiButton.id == 2) {
+				Thread thread = new Thread() {
+					public void run() {
+						runServerPolls();
+					}
+				};
+				thread.start();
+			}
+		}
+	}
+	
+	/**
+	 * Go through all the servers and poll for new info
+	 */
+	private void runServerPolls(){
+		for(Ares_ThreadPollServers temp : serverList){
+			temp.run();
 		}
 	}
 }
---
+++ src/mod/ares/gui/Ares_ThreadPollServers.java
@@ -0,0 +1,156 @@
+package mod.ares.gui;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ConnectException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+import java.net.UnknownHostException;
+
+import net.minecraft.client.gui.GuiMultiplayer;
+import net.minecraft.client.multiplayer.ServerAddress;
+import net.minecraft.network.packet.Packet;
+import net.minecraft.util.ChatAllowedCharacters;
+import net.minecraft.util.MathHelper;
+
+public class Ares_ThreadPollServers extends Thread {
+
+	public static String MOTD = "???";
+	public String gameVersion = "???";
+	public String serverIP = "";
+	public int port = 25665;
+	public String populationInfo = "???";
+	public String pingToServer = "???";
+
+	public Ares_ThreadPollServers(String server, int port){
+		this.serverIP=server;
+		this.port=port;
+	}
+	public void run() {
+			long var1 = System.nanoTime();
+			//get the server info
+			try {
+			pollServer(serverIP,port);
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+            long var3 = System.nanoTime();
+            long temp = (var3 - var1) / 1000000L;
+            pingToServer = String.valueOf(temp);
+	}
+
+	public void pollServer(String server, int port) throws IOException {
+		Socket var2 = null;
+		DataInputStream var3 = null;
+		DataOutputStream var4 = null;
+
+		try {
+			var2 = new Socket();
+			var2.setSoTimeout(3000);
+			var2.setTcpNoDelay(true);
+			var2.setTrafficClass(18);
+			var2.connect(new InetSocketAddress(server, port),
+					3000);
+			var3 = new DataInputStream(var2.getInputStream());
+			var4 = new DataOutputStream(var2.getOutputStream());
+			var4.write(254);
+			var4.write(1);
+
+			if (var3.read() != 255) {
+				throw new IOException("Bad message");
+			}
+
+			String var5 = Packet.readString(var3, 256);
+			char[] var6 = var5.toCharArray();
+
+			for (int var7 = 0; var7 < var6.length; ++var7) {
+				if (var6[var7] != 167
+						&& var6[var7] != 0
+						&& ChatAllowedCharacters.allowedCharacters
+								.indexOf(var6[var7]) < 0) {
+					var6[var7] = 63;
+				}
+			}
+
+			var5 = new String(var6);
+			int var8;
+			int var9;
+			String[] var26;
+			//if server is 1.4.7 get info
+			if (var5.startsWith("\u00a7") && var5.length() > 1) {
+				var26 = var5.substring(1).split("\u0000");
+
+				if (MathHelper.parseIntWithDefault(var26[0], 0) == 1) {
+					MOTD = var26[3];
+					gameVersion = var26[2];
+					var8 = MathHelper.parseIntWithDefault(var26[4], 0);
+					var9 = MathHelper.parseIntWithDefault(var26[5], 0);
+
+					if (var8 >= 0 && var9 >= 0) {
+						populationInfo = "\u00a77" + var8
+								+ "\u00a78/\u00a77" + var9;
+					} else {
+						populationInfo = "\u00a78???";
+					}
+				} else {
+					gameVersion = "???";
+					MOTD = "\u00a78???";
+					populationInfo = "\u00a78???";
+				}
+			} 
+			//if the server is 1.3 get its info
+			else {
+				var26 = var5.split("\u00a7");
+				var5 = var26[0];
+				var8 = -1;
+				var9 = -1;
+
+				try {
+					var8 = Integer.parseInt(var26[1]);
+					var9 = Integer.parseInt(var26[2]);
+				} catch (Exception var24) {
+					;
+				}
+
+				MOTD = "\u00a77" + var5;
+
+				if (var8 >= 0 && var9 > 0) {
+					populationInfo = "\u00a77" + var8
+							+ "\u00a78/\u00a77" + var9;
+				} else {
+					populationInfo = "\u00a78???";
+				}
+
+				gameVersion = "1.3";
+			}
+		} 
+		//close all the open ports to the server
+		finally {
+			try {
+				if (var3 != null) {
+					var3.close();
+				}
+			} catch (Throwable var23) {
+				;
+			}
+
+			try {
+				if (var4 != null) {
+					var4.close();
+				}
+			} catch (Throwable var22) {
+				;
+			}
+
+			try {
+				if (var2 != null) {
+					var2.close();
+				}
+			} catch (Throwable var21) {
+				
+			}
+		}
+	}
+}
