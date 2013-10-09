86b7cf312d43be3826df391d2b8128c70c578cf2
flv92 <flv92400@gmail.com>
2013-10-09 15:29:54 +0200

    Add a way to remove update message if needed

--- undercast/client/update/Undercast_UpdaterThread.java
+++ undercast/client/update/Undercast_UpdaterThread.java
@@ -8,14 +8,14 @@ package undercast.client.update;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
+
+import undercast.client.UndercastConfig;
 import undercast.client.UndercastData;
 import undercast.client.UndercastModClass;
 
 public class Undercast_UpdaterThread extends Thread {
-
     boolean errorOccured;
     public static boolean finished = false;
-    boolean emergencyParser = false; // If we should use the emergency parser
 
     public Undercast_UpdaterThread() {
         errorOccured = false;
@@ -27,15 +27,15 @@ public class Undercast_UpdaterThread extends Thread {
         }
     }
 
-    @Override
     public void run() {
         String readline = "";
         String readline2 = "Could not get update information.";
         String readline3 = "1:2:3:-1";
+        boolean emergencyParser = false; // If we should use the emergency parser
         errorOccured = false;
         try {
             // download link
-            URL data = new URL("https://raw.github.com/UndercastTeam/UndercastClient/forge/version.txt");
+            URL data = new URL("https://raw.github.com/UndercastTeam/UndercastClient/master/version.txt");
             final BufferedReader in = new BufferedReader(new InputStreamReader(data.openStream()));
             readline = in.readLine();
             readline2 = in.readLine();
@@ -47,13 +47,25 @@ public class Undercast_UpdaterThread extends Thread {
             UndercastData.setUpdate(false);
             UndercastData.setUpdateLink("Could not get update information.");
         }
-        if (!UndercastModClass.MOD_VERSION.contains("dev") && compareVersions(readline)) {
-            UndercastData.setUpdate(false);
-            if (!errorOccured) {
-                UndercastData.setUpdateLink(readline2);
-            } else {
-                UndercastData.setUpdateLink("An unknown error occured while getting the update information.");
+        // If the user enters the latest version in the config he won't
+        // notifications for this version
+        if (!UndercastConfig.ignoreVersionUpdateMessage.equals(readline)) {
+            // prevent the user from skipping the next version in advance
+            // = reset the setting if it doesn't match
+            if (!UndercastConfig.ignoreVersionUpdateMessage.equals("0.0.0")) {
+                UndercastConfig.setStringProperty("ignoreVersionUpdateMessage", "0.0.0");
+                System.out.println("[UndercastMod]: The setting ignoreVersionUpdateMessage was reset.\nYou may only enter the latest version number.");
             }
+            if (!UndercastModClass.MOD_VERSION.contains("dev") && compareVersions(readline)) {
+                UndercastData.setUpdate(false);
+                if (!errorOccured) {
+                    UndercastData.setUpdateLink(readline2);
+                } else {
+                    UndercastData.setUpdateLink("An unknown error occured while getting the update information.");
+                }
+            }
+        } else {
+            System.out.println("[UndercastMod]: The update check was skipped");
         }
         if (readline3 != null) {
             try {
