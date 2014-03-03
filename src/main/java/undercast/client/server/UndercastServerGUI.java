de1d96b1b115c11eb4088cb92befa800d30d1443
flv92 <florian.veaux@laposte.net>
2014-03-03 18:38:07 +0100

    Stop spamming screen if server is down

--- src/main/java/undercast/client/server/UndercastServerGUI.java
+++ src/main/java/undercast/client/server/UndercastServerGUI.java
@@ -192,8 +192,6 @@ public class UndercastServerGUI extends GuiScreen {
                     UndercastModClass.getInstance().connection.connect();
                     NetManager.sendPacket(new Packet00Authentication(username, "UndercastClient-v" + UndercastModClass.MOD_VERSION));
                 } catch (Exception e) {
-                    UndercastAchievement a = new UndercastAchievement(username, "\u00A74Server", "\u00A74down!");
-                    UndercastModClass.getInstance().guiAchievement.queueTakenAchievement(a);
                 }
 
             }
